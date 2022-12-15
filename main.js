f7c428e5afe31cc25742e631db83f4c2b8f1746c
davey-mwaura <davidmburu50@gmail.com>
2022-12-15 22:55:26 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 // Defining text characters for the empty and full hearts for you to use later.
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
 // Your JavaScript code goes here!
+let like = document.getElementsByClassName("like")
+console.log(like)
+like.addEventListener("click",mimicServerCall)
 
 
+
+// When a user clicks on a full heart
+document.getElementById("full-heart").addEventListener("click", () => {
+  // Change the heart back to an empty heart
+  document.getElementById("full-heart").innerHTML = EMPTY_HEART;
+  // Remove the .activated-heart class
+  document.getElementById("full-heart").classList.remove("activated-heart");
+});
 
 
 //------------------------------------------------------------------------------
@@ -23,3 +36,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
